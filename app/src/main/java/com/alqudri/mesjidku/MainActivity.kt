fdb59971f859661a20fa67ca2314e4cc60358a6f
Al Qudri <ayiz_60@yahoo.com>
2020-09-08 22:12:09 +0700

    inintial commit

---
+++ app/src/main/java/com/alqudri/mesjidku/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alqudri.mesjidku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
